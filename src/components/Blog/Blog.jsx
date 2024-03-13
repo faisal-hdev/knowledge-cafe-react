1f4f15e33efe902fb147ab5706f152eacc3a4674
faisal-hdev <itsfaisalchowdhury@gmail.com>
2024-03-13 12:47:57 +0600

    Display layout and  card information

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,60 @@
-// import React from "react";
 import PropTypes from "prop-types";
 
 const Blog = ({ blog }) => {
-  console.log(blog);
-  return <div></div>;
+  const {
+    cover_image,
+    title,
+    author,
+    author_img,
+    posted_date,
+    reading_time,
+    hashtags,
+  } = blog;
+  return (
+    <div>
+      <img
+        className="rounded-2xl"
+        src={cover_image}
+        alt={`Cover picture of the title ${title}`}
+      />
+      <div className="flex flex-col lg:flex-row justify-between mt-8 mb-4">
+        <div className="flex gap-6">
+          <div>
+            <img
+              className="w-14 h-14 object-cover rounded-2xl "
+              src={author_img}
+              alt={`author img of the card`}
+            />
+          </div>
+          <div>
+            <h3 className="font-bold text-lg md:text-xl">Author : {author}</h3>
+            <h3 className="text-gray-500 font-medium">{posted_date}</h3>
+          </div>
+        </div>
+        <div>
+          <h3 className="text-gray-500 text-[20px] font-medium">
+            {reading_time} min read
+          </h3>
+        </div>
+      </div>
+      <h3 className="text-black text-xl md:text-3xl lg:text-[40px] my-4 font-bold">
+        {title}
+      </h3>
+      <p className="mt-4 mb-5">
+        {hashtags.map((hash, idx) => (
+          <span key={idx}>
+            <a className="ml-3 text-lg text-gray-500 font-normal" href="">
+              #{hash}
+            </a>
+          </span>
+        ))}
+      </p>
+      <button className="text-[#6047EC] hover:text-[#735ee6] hover:border-black duration-200 font-semibold text-xl border-b-2">
+        Mark as read
+      </button>
+      <hr className="mb-10 mt-8" />
+    </div>
+  );
 };
 
 Blog.propTypes = {
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
@@ -10,8 +11,11 @@ const Blogs = () => {
   }, []);
 
   return (
-    <div className="md:w-2/3 bg-green-100">
+    <div className="md:w-2/3">
       <h2 className="text-2xl">Blogs : {blogs.length}</h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
     </div>
   );
 };
